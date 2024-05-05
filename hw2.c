461118fe3a78aff87db0b422e1617473981f617c
E1B04 <u11202204@o365.ttu.edu.tw>
2024-05-05 16:28:17 +0800

    遇到困難

--- hw2.c
+++ hw2.c
@@ -46,11 +46,44 @@ int main(void){
 }
 system("PAUSE");
 system("CLS");
-    printf("+++++++++++++++++++++++++++++++++++\n");
+    while(1)
+{
+	printf("+++++++++++++++++++++++++++++++++++\n");
     printf("/   a. 劃出直角三角形             /\n");
     printf("/   b. 顯示乘法表                 /\n");
     printf("/   c. 結束                       /\n");
     printf("+++++++++++++++++++++++++++++++++++\n");
     
+    char c,input;
+    printf("請輸入一個字元\n");
+    fflush(stdin);
+    scanf("%c",&c);
+    if(c=='A'||c=='a')
+    {
+    	system("CLS");
+    	printf("請輸入字元a~n\n");
+    	fflush(stdin);
+    	scanf("%c",&input);
+    	if(input<'a'&&input>'n')
+    	{
+    		printf("錯誤，請重新輸入");
+    		return 1;
+		}
+		int height=input-'a'+1;
+		int i,j,k;
+		for(i=1;i<=height;++i)
+		{
+			for(j=0;j<height-i;++j)
+			{
+				printf(" ");
+			}
+			for(k=0;k<i;++k)
+			{
+				printf("%c",input-k);
+			}
+			printf("\n");
+		}
+	}
+}
 	return 0;
 } 
