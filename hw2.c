b51bc52584bd2befffdc08af43a4c9087a1ae5c9
E1B04 <u11202204@o365.ttu.edu.tw>
2024-05-05 15:33:02 +0800

    讚

--- hw2.c
+++ hw2.c
@@ -2,48 +2,55 @@
 #include<stdlib.h>
 
 int main(void){
-	printf("                ████████            █████████               \n");
-	printf("             ████      ███         █         ██             \n");
-	printf("             ██          ███       █         ███            \n");
-	printf("           ██              ████████             ███         \n");
-	printf("          ██                                       █        \n");
-	printf("        ██                                          ██      \n");
-	printf("       ██          █████          █████               ██    \n");
-	printf("      ██           █████          █████                █    \n");
-	printf("     ██            █████          █████                █    \n");
-	printf("     █             █████          █████                ██   \n");
-	printf("    ██                                                  █   \n");
-	printf("    █                                                   █   \n");
-	printf("    █                                                   █   \n");
-	printf("   █                                                    ██  \n");
-	printf("   █                                                     █  \n");
-	printf("   █                                                     █  \n");
-	printf("   █              ██                   ██                █  \n");
-	printf("   █               ██                 ██                ██  \n");
-	printf("   ██                ████          ████                 █   \n");
-	printf("    █                █   ██████████   █                ██   \n");
-	printf("     █               █                ██              ██    \n");
-	printf("     ██             █                  █              █     \n");
-	printf("      ██          ██                    ██          ██      \n");
-	printf("        ███████████                       ██████████        \n");
-	system("PAUSE");
-	
-	int password; //宣告密碼 
-	int i;
-	for(i=0;i<3;i++)
-	{
-	    printf("請輸入四位數字密碼\n");
-	    scanf("%d",&password);
-	if(password!=2024)  //若密碼不正確 
-	{
-		printf("\a\n錯誤");
-	}  
-	else               //若密碼正確  
-	{
-		printf("歡迎");
-		return 0;
-	}
-	
+    printf("                ████████            █████████               \n");
+    printf("             ████      ███         █         ██             \n");
+    printf("             ██          ███       █         ███            \n");
+    printf("           ██              ████████             ███         \n");
+    printf("          ██                                       █        \n");
+    printf("        ██                                          ██      \n");
+    printf("       ██          █████          █████               ██    \n");
+    printf("      ██           █████          █████                █    \n");
+    printf("     ██            █████          █████                █    \n");
+    printf("     █             █████          █████                ██   \n");
+    printf("    ██                                                  █   \n");
+    printf("    █                                                   █   \n");
+    printf("    █                                                   █   \n");
+    printf("   █                                                    ██  \n");
+    printf("   █                                                     █  \n");
+    printf("   █                                                     █  \n");
+    printf("   █              ██                   ██                █  \n");
+    printf("   █               ██                 ██                ██  \n");
+    printf("   ██                ████          ████                 █   \n");
+    printf("    █                █   ██████████   █                ██   \n");
+    printf("     █               █                ██              ██    \n");
+    printf("     ██             █                  █              █     \n");
+    printf("      ██          ██                    ██          ██      \n");
+    printf("        ███████████                       ██████████        \n");
+    system("PAUSE");
+    
+    int password; //宣告密碼 
+    int i;
+    for(i=0;i<3;i++){
+        printf("請輸入四位數字密碼\n");
+        scanf("%d",&password);
+    if(password!=2024){
+        printf("\a\n錯誤");
+        if(i==2){
+            return 0;
+        }
+    }
+    else{
+        printf("歡迎");
+        break;
+    }
 }
-	
+system("PAUSE");
+system("CLS");
+    printf("+++++++++++++++++++++++++++++++++++\n");
+    printf("/   a. 劃出直角三角形             /\n");
+    printf("/   b. 顯示乘法表                 /\n");
+    printf("/   c. 結束                       /\n");
+    printf("+++++++++++++++++++++++++++++++++++\n");
+    
+	return 0;
 } 
