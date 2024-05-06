f36402011480be89aa0706d9ae5d66e69c07f64d
E1B04 <u11202204@o365.ttu.edu.tw>
2024-05-06 19:29:38 +0800

    成功了

--- hw2.c
+++ hw2.c
@@ -54,32 +54,28 @@ system("CLS");
     printf("/   c. 結束                       /\n");
     printf("+++++++++++++++++++++++++++++++++++\n");
     
-    char c,input,end;
+    int number;
+	char c,input,end;
     printf("請輸入一個字元\n");
     fflush(stdin);
     scanf("%c",&c);
-    if(c=='A'||c=='a')
-    {
+    if(c=='A'||c=='a'){
     	system("CLS");
     	printf("請輸入字元a~n\n");
     	fflush(stdin);
     	scanf("%c",&input);
-    	if(input<'a'&&input>'n')
-    	{
-    		printf("錯誤，請重新輸入");
-    		return 1;
+    	if(input<'a'||input>'n'){
+    		printf("錯誤，請重新輸入\n");
+    		continue;
 		}
 		int height=input-'a'+1;
 		int i,j,k;
-		for(i=1;i<=height;++i)
-		{
-			for(j=0;j<height-i;++j)
-			{
+		for(i=1;i<=height;++i){
+			for(j=0;j<height-i;++j){
 				printf(" ");
 			}
-			for(k=0;k<i;++k)
-			{
-				printf("%c",input-k);
+			for(k=i;k>0;--k){
+				printf("%c",input-k+1);
 			}
 			printf("\n");
 		}
@@ -89,11 +85,17 @@ system("CLS");
 		system("CLS");
     	printf("請輸入字元1~9\n");
     	fflush(stdin);
-    	scanf("%d",&input);
-    	if(input<'1'&&input>'9')
+    	scanf("%d",&number);
+    	if(number<1||number>9)
     	{
-    		printf("錯誤，請重新輸入");
-    		printf("請輸入字元1~9\n");
+    		printf("錯誤，請重新輸入\n");
+    		continue;
+		}
+		else{
+			int num;
+			for(num=1;num<=number;num++){
+			printf("%d*%d=%d\n",num,num,num*num);
+			}
 		}
 	}
 	else if(c=='C'||c=='c')
@@ -114,6 +116,7 @@ system("CLS");
 		else
 		{
 			printf("錯誤，請重新輸入\n");
+			continue;
 		}
 		}	
 	
