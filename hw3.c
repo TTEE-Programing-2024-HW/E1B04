b125dbd53ede87d6b727cd9de11806aea83ccccb
E1B04 <u11202204@o365.ttu.edu.tw>
2024-05-19 13:25:13 +0800

    淚

--- hw3.c
+++ hw3.c
@@ -12,13 +12,18 @@ void Seats(char seats[9][9]) {
         printf("\n");
     }
 }
-	void RandomSeats(char seats[9][9]) {
+	void RandomSeats(char seats[9][9], int numBookings) {
     // Seed the random number generator
     srand(time(0));
 
-    for (i = 0; i < 9; ++i) {
-        int random = rand() % 9;
-        seats[i][random] = '*';
+    int booked = 0;
+    while (booked < numBookings) {
+        int row = rand() % 9;
+        int col = rand() % 4;
+        if (seats[row][col] == '-') {
+            seats[row][col] = '*';
+            booked++;
+        }
     }
 }	
 int main(void){
@@ -54,7 +59,7 @@ int main(void){
         printf("請輸入四位數字密碼\n");
         scanf("%d",&password);
     if(password!=2024){
-        printf("\a\n錯誤");
+        printf("\a\n錯誤\n");
         if(i==2){
             return 0;
         }
@@ -66,47 +71,55 @@ int main(void){
 }
 system("PAUSE");
 system("CLS");
+	char seats[9][9] = {
+        {'-', '-', '-', '-', '-', '-', '-', '-', '-'},
+        {'-', '-', '-', '-', '-', '-', '-', '-', '-'},
+        {'-', '-', '-', '-', '-', '-', '-', '-', '-'},
+        {'-', '-', '-', '-', '-', '-', '-', '-', '-'},
+        {'-', '-', '-', '-', '-', '-', '-', '-', '-'},
+        {'-', '-', '-', '-', '-', '-', '-', '-', '-'},
+        {'-', '-', '-', '-', '-', '-', '-', '-', '-'},
+        {'-', '-', '-', '-', '-', '-', '-', '-', '-'},
+        {'-', '-', '-', '-', '-', '-', '-', '-', '-'},
+	};
+	RandomSeats(seats, 10);
 while(1)//開始進入主選單 
 {
 	printf("+++++++++++++++++++++++++++++++++++\n");
-    printf("/   a. Available seats            /\n");
-    printf("/   b. Arrange for you            /\n");
-    printf("/   c. Choose by yourself         /\n");
-    printf("/   d. Exit                       /\n");
+    printf("/        Main   menu              /\n");
+	printf("/     a. Available seats          /\n");
+    printf("/     b. Arrange for you          /\n");
+    printf("/     c. Choose by yourself       /\n");
+    printf("/     d. Exit                     /\n");
     printf("+++++++++++++++++++++++++++++++++++\n");
     
-    char input,end;
+    int n;
+	char input,end,initialized = 0;
     printf("請輸入a,b,c,d\n");
     fflush(stdin);
     scanf("%c",&input);
-    if(input=='a'){
-    	system("CLS");
-    char seats[9][9] = {
-        {'-', '-', '-', '-', '-', '-', '-', '-', '-'},
-        {'-', '-', '-', '-', '-', '-', '-', '-', '-'},
-        {'-', '-', '-', '-', '-', '-', '-', '-', '-'},
-        {'-', '-', '-', '-', '-', '-', '-', '-', '-'},
-        {'-', '-', '-', '-', '-', '-', '-', '-', '-'},
-        {'-', '-', '-', '-', '-', '-', '-', '-', '-'},
-        {'-', '-', '-', '-', '-', '-', '-', '-', '-'},
-        {'-', '-', '-', '-', '-', '-', '-', '-', '-'},
-        {'-', '-', '-', '-', '-', '-', '-', '-', '-'},
-		};
-    RandomSeats(seats);
-    	if (input == 'a') {
-        Seats(seats);
-        system("PAUSE");
-		system("CLS");
-		} 
-		else {
-        printf("輸入無效。\n");
-		}
-	}
+    if (input == 'a') {
+            if (!initialized) {
+                // Seed the random number generator
+                srand(time(0));
+                // Generate random seats once
+                initialized = 1;
+            }
+            Seats(seats);
+            system("PAUSE");
+			system("CLS");
+    } 
 	else if(input == 'b'){
+		printf("請問是1~4位?\n");
+		scanf("%d",&n);
+		if(n>=1||n<=3){
+			
+		}
 	}
 	else if(input == 'c'){ 
 	}
-	else if(input == 'd'){system("CLS");
+	else if(input == 'd'){
+		system("CLS");
 		printf("Continue?(y/n)\n");
 		fflush(stdin);
 		scanf("%c",&end);
@@ -126,6 +139,12 @@ while(1)//開始進入主選單
 			printf("錯誤，請重新輸入\n");
 			continue;
 		}
+	}
+	else{
+		printf("錯誤，請重新輸入\n");
+		system("PAUSE");
+		system("CLS");
+		continue;
 	}
 }
     return 0;
